3d5c17632d07ed26e199621085e0fda080ff02bd
RogerLiao0001 <r995022526651@gmail.com>
2025-06-11 00:05:47 +0800

    123

--- project_web-main/app.py
+++ project_web-main/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder='templates',
+            static_folder='app/static')
 
 @app.route('/')
 def home():
