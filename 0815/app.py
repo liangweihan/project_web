d508c458872408ddb2db1cffdb2f7a34e542907f
liangweihan <liangweihan93@gmail.com>
2025-09-19 11:14:16 +0800

    update

--- 0815/app.py
+++ 0815/app.py
@@ -6,10 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/group")
-def group():
-    return render_template("group.html")
+@app.route('/group1_1')
+def group1_1():
+    return render_template('group1_1.html')
+
+@app.route('/group1_2')
+def group1_2():
+    return render_template('group1_2.html')
+
+@app.route('/group2_1')
+def group2_1():
+    return render_template('group2_1.html')
+
+@app.route('/group2_2')
+def group2_2():
+    return render_template('group2_2.html')
 
 if __name__ == '__main__':
-    app.run(port=8080,debug=True)
-
+    app.run(port=5000,debug=True)
